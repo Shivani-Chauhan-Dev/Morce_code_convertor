015ce2fb8ad4ba8996723730f07fccafb6b0708e
Shivani Chauhan <shivanichauhandev.@gmail.com>
2024-02-20 12:18:28 +0530

    added morce code file

---
+++ morce_code_convertor.py
@@ -0,0 +1,67 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+          cipher += ' '
+          return cipher
+ 
+def decrypt(message):
+    message += ' '
+ 
+    decipher = ''
+    citext = ''
+    for letter in message:
+        if (letter != ' '):
+ 
+            # counter to keep track of space
+            i = 0
+ 
+            # storing morse code of a single character
+            citext += letter
+ 
+        # in case of space
+        else:
+            # if i = 1 that indicates a new character
+            i += 1
+ 
+            # if i = 2 that indicates a new word
+            if i == 2 :
+ 
+                 # adding space to separate words
+                decipher += ' '
+            else:
+ 
+                # accessing the keys using their values (reverse of encryption)
+                decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
+                .values()).index(citext)]
+                citext = ''
+ 
+    return decipher
+def main():
+    message = input("enter the text from the user:")
+    result = encrypt(message.upper())
+    print (result)
+ 
+    message = input("enter the text from the user: ")
+    result = decrypt(message)
+    print (result)
+if __name__ == '__main__':
+    main()
